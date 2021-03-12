f3b4501528c2bac6aab512994d745487c82717db
Ricardo Sacramento <ricardo_sacramento@outlook.com>
2021-03-12 20:26:07 +0000

    adding new css to app

--- site/src/components/creategame.tsx
+++ site/src/components/creategame.tsx
@@ -7,19 +7,17 @@ const CreateGame = (props: any) => {
   const onSubmit = (data: any) => props.socket.emit("createGame", data);
 
   return (
-    <div className="bg-none flex h-screen">
-      <div className="bg-white bg-pattern w-3/4 m-auto rounded-sm py-8 flex align-center flex-col justify-center">
-        <div className="text-center">
-          <h1 className="text-4xl">Create Game</h1>
-        </div>
-        <div className="flex justify-center">
-          <form className="flex flex-col justify-center text-center mt-4 p-4 w-3/4" onSubmit={handleSubmit(onSubmit)}>
-            <input className="my-1 w-44 border border-black rounded-sm p-2" type="number" name="playerCount" ref={register} placeholder="Number of players"/>
-            <input className="my-1 w-44 border border-black rounded-sm p-2" type="number" name="roundTime" ref={register} placeholder="Round time"/>
-            <input className="my-1 w-44 border border-black rounded-sm p-2" type="text" name="playerName" ref={register} placeholder="Player Name"/>
-            <input className="text-white font-bold p-4 my-1 w-44 bg-blue-500" type="submit" value="Create Game"/>
-          </form>
-        </div>
+    <div className="flex flex-col h-screen py-4">
+      <div className="text-gray-400 text-center mt-16 text-4xl uppercase">
+        <h1 className="text-4xl">Create Game</h1>
+      </div>
+      <div className="bg-primary my-14 mx-auto w-2/3 p-6 rounded bg-opacity-80">
+        <form className="flex flex-col justify-center text-center p-4 w-full" onSubmit={handleSubmit(onSubmit)}>
+          <input className="bg-background p-2 my-2 rounded border text-gray-400" type="number" name="playerCount" ref={register} placeholder="Number of players"/>
+          <input className="bg-background p-2 my-2 rounded border text-gray-400" type="number" name="roundTime" ref={register} placeholder="Round time"/>
+          <input className="bg-background p-2 my-2 rounded border text-gray-400" type="text" name="playerName" ref={register} placeholder="Player Name"/>
+          <input className="w-full text-center font-medium bg-button text-white p-2 rounded my-2" type="submit" value="Create Game"/>
+        </form>
       </div>
     </div>
   );
--- site/src/components/init.tsx
+++ site/src/components/init.tsx
@@ -13,14 +13,14 @@ const Init = (props: any) => {
         <div className="text-white text-center mt-16 text-4xl uppercase">
           <h1 className="">Spyfall</h1>
         </div>
-        <div className="bg-primary my-4 my-14 mx-auto w-2/3 p-6 rounded bg-opacity-80">
+        <div className="bg-primary my-14 mx-auto w-2/3 p-6 rounded bg-opacity-80">
           <button className="w-full text-center font-medium bg-button text-white p-4 rounded" onClick={createGame}>Create New Game</button>
         </div>
         <div className="bg-primary my-4 mt-40 mx-auto w-2/3 p-6 rounded bg-opacity-80">
-          <form className="" onSubmit={handleSubmit(joinGame)}>
-            <input className="" type="submit" value="Join Game"/>
-            <input className="" type="text" name="playerName" ref={register} placeholder="Player Name" />
-            <input className="" type="text" name="gameCode" ref={register} placeholder="Room Name" />
+          <form className="flex flex-col" onSubmit={handleSubmit(joinGame)}>
+            <input className="w-full text-center font-medium bg-button text-white p-2 rounded my-2" type="submit" value="Join Game"/>
+            <input className="bg-background p-2 my-2 rounded border text-gray-400" type="text" name="playerName" ref={register} placeholder="Player Name" />
+            <input className="bg-background p-2 my-2 rounded border text-gray-400" type="text" name="gameCode" ref={register} placeholder="Room Name" />
           </form>
         </div>
     </div>
